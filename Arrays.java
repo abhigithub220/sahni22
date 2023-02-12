8181fc568dd02e839dc3011ae69ddab22e517f17
abhigithub220 <125180030+abhigithub220@users.noreply.github.com>
2023-02-12 13:24:55 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String args[]){
+        // int[] marks = new int[3];
+        // int marks[] = new int[3];
+        int marks[] = {95, 96,97};
+        // marks[0] = 95; //phy
+        // marks[1] = 96; //chem
+        // marks[2] = 97; //eng
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
